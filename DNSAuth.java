b305df63255185fc1eb7ce6c1b3c1f815eef02a1
srijeet2310 <53825099+srijeet2310@users.noreply.github.com>
2021-11-24 21:32:07 +0530

    Created DNSAuth.java file (Has main)

---
+++ DNSAuth.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class DNSAuth
+{
+    public static void main(String args[])
+    {
+        Server ser = new Server();
+        Client cl1 = new Client("182.0.1.2", ser, "162.0.1.2");
+        Client cl2 = new Client("162.0.1.2", ser, "182.0.1.2");
+        //Parametrs passed for the client objects are : (Actual IPAddress, Server Object, Spoofed IPAddress)
+        /*
+         * Note that the Client can have any randomly generated spoofed IPAddress but for 
+         * demostration purposes we have choosen to hardcode the spoofed IPAddress such that multi-socket
+         * programming can be demostrated in the video. 
+         * Multi-socket programming allows multiple clients to request for information from the 
+         * same server and their requests are resolved using multi-threading in runtime.
+         */
+        System.out.println("Client - Server Architecture created");
+        cl1.sendRequestPacket("Need Information",0); //Correct Request
+        cl1.sendRequestPacket("Need Information",1); //Spoofed Request
+        //SendRequestPacket has parameters : (Message string, 0/1) : 
+        //0 stands for correct request and 1 stands for spoofed request
+    }
+}
